f64f72e34c9d4378bc7be61f539bf5ef82fdb576
Sargeras <90414573+Lasarest@users.noreply.github.com>
2023-03-02 12:02:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importing packages bs4 and requests, input url
 from bs4 import BeautifulSoup
 import requests
 url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
@@ -5,8 +6,10 @@ page = requests.get(url)
 filteredFilms = []
 allFilms = []
 allRating = []
+# Getting code from page
 soup = BeautifulSoup(page.text, "html.parser")
 allFilms = soup.findAll('td', class_='titleColumn')
+# Getting text from tag "a"
 for data in allFilms:
     if data.find('a') is not None:
         filteredFilms.append(data.text)
@@ -16,6 +19,7 @@ for data in filteredFilms:
     data = filtered[:-8]
     allFilms.append(data)
 filteredFilms.clear()
+# Getting film rating from tag "tb"
 allRating = soup.findAll('td', class_='ratingColumn imdbRating')
 for data in allRating:
     if data.find('strong') is not None:
@@ -24,5 +28,6 @@ allRating.clear()
 for data in filteredFilms:
     filtered = data[1:4]
     allRating.append(filtered)
+# Joining two dictionaries
 Result_Dictionary = dict(zip(allFilms,allRating))
 print(Result_Dictionary)
